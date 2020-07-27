751ce2b0a2dca17313df926a0f92e8400e5d5d6e
mmoya26 <mmoya18@icloud.com>
2020-07-27 15:35:34 -0500

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Miguel Moya on 7/27/20.
+//  Copyright © 2020 Miguel Moya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
